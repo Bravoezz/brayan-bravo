b2fc89bbb5bbabec6e90560194b11a05d7635d02
Bravoezz <joelmrd1234@gmail.com>
2024-08-22 04:26:35 -0500

    add config to deploy

---
+++ test/unit/product/product.service.spec.ts
@@ -0,0 +1,21 @@
+/**
+import { ProductService } from '../../../src/modules/product/product.service';
+import { PrismaService } from '../../../src/modules/prisma/prisma.service';
+import { Test, TestingModule } from '@nestjs/testing';
+
+
+describe('Module Product - Service', () => {
+	let productService: ProductService;
+	let prismaService: PrismaService;
+
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [
+				ProductService,
+				PrismaService
+			]
+		})
+	})
+})
+
+ **/
